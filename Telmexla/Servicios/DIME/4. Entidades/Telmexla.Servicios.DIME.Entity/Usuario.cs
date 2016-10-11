ccbe7d455503d3e938b1ac3dbb37564c75f8db68
Alchaz <alchaztaz@gmail.com>
2016-10-11 15:56:47 -0500

    actualizacion

--- Telmexla/Servicios/DIME/4. Entidades/Telmexla.Servicios.DIME.Entity/Usuario.cs
+++ Telmexla/Servicios/DIME/4. Entidades/Telmexla.Servicios.DIME.Entity/Usuario.cs
@@ -9,8 +9,6 @@
 // TargetFrameworkVersion = 4.51
 #pragma warning disable 1591    //  Ignore "Missing XML Comment" warning
 
-using System.ComponentModel.DataAnnotations;
-
 namespace Telmexla.Servicios.DIME.Entity
 {
 
@@ -18,14 +16,16 @@ namespace Telmexla.Servicios.DIME.Entity
     public class Usuario
     {
         public int Id { get; set; } // ID (Primary key)
-
-        [Display(Name="Cedula de Ciudadania")]
         public decimal? Cedula { get; set; } // CEDULA
-        public string Nombre { get; set; } // NOMBRE (length: 20)
+        public string Nombre { get; set; } // NOMBRE (length: 100)
         public string Contrasena { get; set; } // CONTRASENA (length: 300)
+        public string Genero { get; set; } // GENERO (length: 1)
+        public string Correo { get; set; } // CORREO (length: 200)
+        public System.DateTime? FechaNacimiento { get; set; } // FECHA_NACIMIENTO
+        public decimal? Telefono { get; set; } // TELEFONO
 
         // Reverse navigation
-        public virtual System.Collections.Generic.ICollection<RegistroSesion> RegistroSesions { get; set; } // TBL_REGISTRO_SESION.FK__TBL_REGIS__ID_US__37703C52
+        public virtual System.Collections.Generic.ICollection<RegistroSesion> RegistroSesions { get; set; } // TBL_REGISTRO_SESION.FK__TBL_REGIS__ID_US__50FB042B
         public virtual System.Collections.Generic.ICollection<UsuariosXAcceso> UsuariosXAccesoes { get; set; } // Many to many mapping
         public virtual System.Collections.Generic.ICollection<UsuariosXPreguntasDesb> UsuariosXPreguntasDesbs { get; set; } // Many to many mapping
 
