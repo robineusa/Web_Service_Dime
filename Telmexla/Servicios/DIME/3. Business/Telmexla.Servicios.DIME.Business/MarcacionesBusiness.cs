fc4ab85d5a9b6d08113ddb1213085c0a1a5e46cc
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2016-12-01 16:03:18 -0500

    Cambios Marcacion con Julio

---
+++ Telmexla/Servicios/DIME/3. Business/Telmexla.Servicios.DIME.Business/MarcacionesBusiness.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telmexla.Servicios.DIME.Data;
+using Telmexla.Servicios.DIME.Data.Context;
+using Telmexla.Servicios.DIME.Entity;
+
+namespace Telmexla.Servicios.DIME.Business
+{
+    public class MarcacionesBusiness
+    {
+        public void RegistrarNuevaMarcacion(MaestroMarcacione marcacion) {
+            UnitOfWork unitOfWork = new UnitOfWork(new DimeContext());
+        
+        }
+    }
+}
