d2854e1ca5d49961c37e1f8a60b205e648ace778
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-04-19 16:02:57 -0500

    Commit

--- Telmexla/Servicios/DIME/1. Ejecucion de WebService/Telmexla.Servicios.DIME.Ejecutor/Program.cs
+++ Telmexla/Servicios/DIME/1. Ejecucion de WebService/Telmexla.Servicios.DIME.Ejecutor/Program.cs
@@ -50,15 +50,15 @@ namespace Telmexla.Servicios.DIME.Ejecutor
             //    //MaestroNodoService servi = new MaestroNodoService();
             //    //servi.InsertarNodo(nodo);
 
-            //UsabilidadConvenioInbound model = new UsabilidadConvenioInbound();
-            //model.Id = 0;
-            //model.IdUsuarioRevision = 78;
-            //model.NombreUsuarioRevision = "Robin la perra";
-            //model.CuentaRevisoTabla = 307;
+            UsabilidadConvenioInbound model = new UsabilidadConvenioInbound();
+            model.Id = 0;
+            model.IdUsuarioRevision = 78;
+            model.NombreUsuarioRevision = "David la perra";
+            model.CuentaRevisoTabla = 307;
 
-            //UsabilidadService U = new UsabilidadService();
-            //U.InsertarUsabilidadInboundConvenio(model);
-            //var z = "";
+            UsabilidadService U = new UsabilidadService();
+            U.InsertarUsabilidadInboundConvenio(model);
+            var z = "";
 
 
             //    //GraficosBusiness BUSI = new GraficosBusiness();
--- Telmexla/Servicios/DIME/2. WebServices/Telmexla.Servicios.DIME.WebServices/MainWebService/WebService.cs
+++ Telmexla/Servicios/DIME/2. WebServices/Telmexla.Servicios.DIME.WebServices/MainWebService/WebService.cs
@@ -12,7 +12,7 @@ using Telmexla.Servicios.DIME.Helpers.Mappers;
 
 namespace Telmexla.Servicios.DIME.WebServices.MainWebService
 {
-    public class WebService : IWebService, ILoginService, IInboundService, IMaestrosService, IBlendingService,IMarcacionesService, INotificacionesBuenServicioService, ICasosCelulaService, ICasosAdminService,ITrasladosService,IMaestroNodoService,IActivacionClaroVideoService,IActivacionSiembraHDService,IBalanceScoreCardService, IGraficosService
+    public class WebService : IWebService, ILoginService, IInboundService, IMaestrosService, IBlendingService,IMarcacionesService, INotificacionesBuenServicioService, ICasosCelulaService, ICasosAdminService,ITrasladosService,IMaestroNodoService,IActivacionClaroVideoService,IActivacionSiembraHDService,IBalanceScoreCardService, IGraficosService,IUsabilidadService
     {
 
 
