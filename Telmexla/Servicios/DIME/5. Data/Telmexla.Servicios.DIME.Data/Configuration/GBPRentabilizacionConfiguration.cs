d039d7854f73547a99a283d9c80e18a918caa51f
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-06-08 21:31:22 +0200

    Rentabilizacion

---
+++ Telmexla/Servicios/DIME/5. Data/Telmexla.Servicios.DIME.Data/Configuration/GBPRentabilizacionConfiguration.cs
@@ -0,0 +1,39 @@
+﻿#pragma warning disable 1591    //  Ignore "Missing XML Comment" warning
+
+using Telmexla.Servicios.DIME.Entity;
+
+namespace Telmexla.Servicios.DIME.Data.Configuration
+{
+    public class GBPRentabilizacionConfiguration : System.Data.Entity.ModelConfiguration.EntityTypeConfiguration<GBPRentabilizacion>
+    {
+        public GBPRentabilizacionConfiguration()
+            : this("dbo")
+        { }
+        public GBPRentabilizacionConfiguration(string schema)
+        {
+            ToTable("TBL_GBP_RENTABILIZACIO", schema);
+            HasKey(x => new { x.Id });
+
+            Property(x => x.Id).HasColumnName(@"ID").IsRequired().HasColumnType("numeric").HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
+            Property(x => x.FechaGestion).HasColumnName(@"FECHA_GESTION").IsOptional().HasColumnType("datetime");
+            Property(x => x.UsuarioGestion).HasColumnName(@"USUARIO_GESTION").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.AliadoGestion).HasColumnName(@"ALIADO_GESTION").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.OperacionGestion).HasColumnName(@"OPERACION_GESTION").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.CuentaCliente).HasColumnName(@"CUENTA_CLIENTE").IsOptional().HasColumnType("numeric");
+            Property(x => x.ConsumosPpv).HasColumnName(@"CONSUMOS_PPV").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.UltimaPpv).HasColumnName(@"ULTIMA_PPV").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.SiembraHd).HasColumnName(@"SIEMBRA_HD").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.SiembraVoz).HasColumnName(@"SIEMBRA_VOZ").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.BlindajeInternet).HasColumnName(@"BLINDAJE_INTERNET").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.UltimaMarcacion).HasColumnName(@"ULTIMA_MARCACION").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.OfrecimientoAceptado).HasColumnName(@"OFRECIMIENTO_ACEPTADO").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.TipoContacto).HasColumnName(@"TIPO_CONTACTO").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.Gestion).HasColumnName(@"GESTION").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.Cierre).HasColumnName(@"CIERRE").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.Causa).HasColumnName(@"CAUSA").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.Motivo).HasColumnName(@"MOTIVO").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
+            Property(x => x.Observaciones).HasColumnName(@"OBSERVACIONES").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(1000);
+            Property(x => x.FechaSeguimiento).HasColumnName(@"FECHA_SEGUIMIENTO").IsOptional().HasColumnType("datetime");
+        }
+    }
+}
