b04fd253b80805b3e8fcc01a0ceea293254a3b1f
David1010Gomez <dagomez.15m@gmail.com>
2017-08-02 07:28:46 -0500

    Commit Recurrencia

---
+++ Telmexla/Servicios/DIME/4. Entidades/Telmexla.Servicios.DIME.Entity/MaestroOpcionesRecurrencia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telmexla.Servicios.DIME.Entity
+{
+    public class MaestroOpcionesRecurrencia
+    {
+        public int Id { get; set; }
+        public int Id_Proceso { get; set; }
+        public string Razon { get; set; }
+    }
+}
