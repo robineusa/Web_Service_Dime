9915ff9b19c77b4c0212ff063d3ecc40323dde72
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-08-14 11:00:25 -0500

    Commit Business bitacora

--- Telmexla/Servicios/DIME/3. Business/Telmexla.Servicios.DIME.Business/BitacoraIncidentesBusiness.cs
+++ Telmexla/Servicios/DIME/3. Business/Telmexla.Servicios.DIME.Business/BitacoraIncidentesBusiness.cs
@@ -20,5 +20,81 @@ namespace Telmexla.Servicios.DIME.Business
             unitWork.Complete();
             unitWork.Dispose();
         }
+        public List<BIMGerencias> ListaDeGerencias()
+        {
+            DimeContext dimContext = new DimeContext();
+            List<BIMGerencias> result = new List<BIMGerencias>();
+            var objetosResult = (from a in dimContext.BIMGerencias
+                                 where a.Estado.Equals("ACTIVO")
+                                 orderby a.NombreGerencia ascending
+                                 select new
+                                 {
+                                     a.IdGerencia,
+                                     a.NombreGerencia
+
+                                 }
+                                 ).ToList();
+
+            for (int i = 0; i < objetosResult.Count; i++)
+            {
+                result.Add(new BIMGerencias());
+                result[i].IdGerencia = objetosResult[i].IdGerencia;
+                result[i].NombreGerencia = objetosResult[i].NombreGerencia;
+
+            }
+            return result;
+        }
+        public List<BIMAliados> ListaDeAliados()
+        {
+            DimeContext dimContext = new DimeContext();
+            List<BIMAliados> result = new List<BIMAliados>();
+            var objetosResult = (from a in dimContext.BIMAliados
+                                 where a.Estado.Equals("ACTIVO")
+                                 orderby a.NombreAliado ascending
+                                 select new
+                                 {
+                                     a.IdAliado,
+                                     a.NombreAliado
+
+                                 }
+                                 ).ToList();
+
+            for (int i = 0; i < objetosResult.Count; i++)
+            {
+                result.Add(new BIMAliados());
+                result[i].IdAliado = objetosResult[i].IdAliado;
+                result[i].NombreAliado = objetosResult[i].NombreAliado;
+
+            }
+            return result;
+        }
+        public List<BIMOperaciones> ListaOperaciones()
+        {
+            DimeContext dimContext = new DimeContext();
+            List<BIMOperaciones> result = new List<BIMOperaciones>();
+            var objetosResult = (from a in dimContext.BIMOperaciones
+                                 where a.Estado.Equals("ACTIVO")
+                                 orderby a.NombreOperacion ascending
+                                 select new
+                                 {
+                                     a.IdOperacion,
+                                     a.IdGerencia,
+                                     a.IdAliado,
+                                     a.NombreOperacion
+
+                                 }
+                                 ).ToList();
+
+            for (int i = 0; i < objetosResult.Count; i++)
+            {
+                result.Add(new BIMOperaciones());
+                result[i].IdOperacion = objetosResult[i].IdOperacion;
+                result[i].IdGerencia = objetosResult[i].IdGerencia;
+                result[i].IdAliado = objetosResult[i].IdAliado;
+                result[i].NombreOperacion = objetosResult[i].NombreOperacion;
+
+            }
+            return result;
+        }
     }
 }
