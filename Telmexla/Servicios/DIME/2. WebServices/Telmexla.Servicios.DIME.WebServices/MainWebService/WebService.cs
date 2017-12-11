e17a30db24885be02074e4fe80acd45038f5830e
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-12-11 16:08:38 -0500

    commit

--- Telmexla/Servicios/DIME/2. WebServices/Telmexla.Servicios.DIME.WebServices/MainWebService/WebService.cs
+++ Telmexla/Servicios/DIME/2. WebServices/Telmexla.Servicios.DIME.WebServices/MainWebService/WebService.cs
@@ -3015,6 +3015,11 @@ namespace Telmexla.Servicios.DIME.WebServices.MainWebService
             OfertasComercialesService OfertasService = new OfertasComercialesService();
             return OfertasService.ListaDeImagenesAdmin();
         }
+        public List<IMGOfertasComeciales> ListaDeImagenesActivas()
+        {
+            OfertasComercialesService OfertasService = new OfertasComercialesService();
+            return OfertasService.ListaDeImagenesActivas();
+        }
         #endregion
     }
 }
