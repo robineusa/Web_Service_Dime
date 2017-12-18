89966389d1949a8505bba54e8a5c2c4c618405bb
Robinson Neusa Pinilla <robinson.neusa@gmail.com>
2017-12-18 12:13:55 -0500

    Commit Cambios Entidades Dx

--- Telmexla/Servicios/DIME/4. Entidades/Telmexla.Servicios.DIME.Entity/CEPDesconexiones.cs
+++ Telmexla/Servicios/DIME/4. Entidades/Telmexla.Servicios.DIME.Entity/CEPDesconexiones.cs
@@ -8,20 +8,11 @@
         public string NombreUsuarioGestion { get; set; }
         public string CanalDeIngreso { get; set; }
         public decimal CuentaCliente { get; set; }
-        public string MotivoDeDesconexion { get; set; }
-        public string Codigo { get; set; }
         public string Nota1 { get; set; }
         public string Nota2 { get; set; }
-        public string Nota3 { get; set; }
-        public string Nota4 { get; set; }
-        public string Nota5 { get; set; }
-        public string Red { get; set; }
-        public string Servicios { get; set; }
         public System.DateTime? FechaDeSolicitud { get; set; }
         public System.DateTime? FechaDeCorte { get; set; }
         public System.DateTime? FechaDePreaviso { get; set; }
-        public decimal DiaDeDesconexion { get; set; }
-        public string TarifaAnterior { get; set; }
         public System.DateTime? FechaDeAsignacion { get; set; }
         public string Gestion { get; set; }
         public string Subrazon { get; set; }
