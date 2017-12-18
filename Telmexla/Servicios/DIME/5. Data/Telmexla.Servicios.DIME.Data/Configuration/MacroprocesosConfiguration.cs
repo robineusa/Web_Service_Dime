ca36e75115bb4c302cc65c1102713f05a71686ce
Antonio <antony021santana.96@gmail.com>
2017-12-18 14:15:59 -0500

    commit categoria

--- Telmexla/Servicios/DIME/5. Data/Telmexla.Servicios.DIME.Data/Configuration/MacroprocesosConfiguration.cs
+++ Telmexla/Servicios/DIME/5. Data/Telmexla.Servicios.DIME.Data/Configuration/MacroprocesosConfiguration.cs
@@ -17,7 +17,8 @@ namespace Telmexla.Servicios.DIME.Data.Configuration
             Property(x => x.IdCategoria).HasColumnName(@"ID_CATEGORIA").IsRequired().HasColumnType("int").HasDatabaseGeneratedOption(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity);
             Property(x => x.IdCategoriaPadre).HasColumnName(@"ID_CATEGORIA_PADRE").IsRequired().HasColumnType("int");
             Property(x => x.Descripcion).HasColumnName(@"DESCRIPCION").IsOptional().IsUnicode(false).HasColumnType("varchar").HasMaxLength(255);
-      
+            Property(x => x.IdArbol).HasColumnName(@"ID_ARBOL").IsRequired().HasColumnType("int");
+
         }
 
     }
