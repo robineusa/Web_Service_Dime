1572ea14ad4e98f0ea977a8faec0befc8e9eda96
David1010Gomez <dagomez.15m@gmail.com>
2017-12-19 14:43:11 -0500

    Commit

---
+++ Telmexla/Servicios/DIME/4. Entidades/Telmexla.Servicios.DIME.Entity/CEPSuspensiones.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telmexla.Servicios.DIME.Entity
+{
+    public class CEPSuspensiones
+    {
+        public decimal IdTransaccion { get; set; }
+        public System.DateTime? FechaGestion { get; set; }
+        public decimal UsuarioDeGestion { get; set; }
+        public string NombreUsuarioGestion { get; set; }
+        public string CanalDeIngreso { get; set; }
+        public decimal CuentaCliente { get; set; }
+        public string Nota1 { get; set; }
+        public string Nota2 { get; set; }
+        public System.DateTime? FechaDeSolicitud { get; set; }
+        public System.DateTime? FechaDeCorte { get; set; }
+        public System.DateTime? FechaDePreaviso { get; set; }
+        public System.DateTime? FechaDeAsignacion { get; set; }
+        public string Gestion { get; set; }
+        public string Subrazon { get; set; }
+        public string Estado { get; set; }
+        public System.DateTime? FechaDeSeguimiento { get; set; }
+        public decimal MovieLetter { get; set; }
+        public string Ajuste { get; set; }
+        public decimal CantidadServicio { get; set; }
+        public string ErrorSolicitud { get; set; }
+        public string UsuarioSolicitud { get; set; }
+        public string TipoDeError { get; set; }
+        public string Observaciones { get; set; }
+    }
+}
